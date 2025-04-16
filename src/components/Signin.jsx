a777e7c6047ff371e03fb322c35bd4074d6aecf4
axel <giffard.axel95@gmail.com>
2025-04-16 22:13:53 +0200

    FEAT: changements pour voir

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -16,7 +16,7 @@ function Signin({toLogin}) {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await fetch("https://backendmedicial.onrender.com/register", {
+            const res = await fetch("https://medic-ial.netlify.app/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
